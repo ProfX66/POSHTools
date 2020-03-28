631d3e2ac6a231e4326554e4af53cea7080cda61
ProfX <prof@pxcnet.com>
2020-03-27 19:18:56 -0700

    Get the last power events from event viewer

---
+++ Scripts/Get-PowerEvents.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-PowerEvents()
+{
+	Get-WinEvent -FilterHashtable @{ logname='System'; id=1074 } | ForEach-Object `
+	{
+		[PSCustomObject]@{
+            ID = 1074;
+			Date = $_.TimeCreated; 
+			User = $_.Properties[6].Value; 
+			Process = $_.Properties[0].Value; 
+			Action = $_.Properties[4].Value; 
+			Reason = $_.Properties[2].Value; 
+			ReasonCode = $_.Properties[3].Value;
+			Comment = $_.Properties[5].Value;
+		}
+	}
+}
