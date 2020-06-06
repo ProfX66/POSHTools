407090cc61a1b57ebb00664f9265da92cc2703c5
ProfX <prof@pxcnet.com>
2020-06-06 04:31:50 -0700

    Modernized Get-PowerEvents

--- Scripts/Get-PowerEvents.ps1
+++ Scripts/Get-PowerEvents.ps1
@@ -1,10 +1,20 @@
-﻿Function Get-PowerEvents()
+﻿Function Get-PowerEvents([String]$Computer = $env:COMPUTERNAME, [String]$Path = $null, [TimeSpan]$TimeMask = [TimeSpan]::Zero, [Switch]$Descending)
 {
-	Get-WinEvent -FilterHashtable @{ logname='System'; id=1074 } | ForEach-Object `
+	[Int]$Private:id = 1074
+	[String]$Private:source = $Computer
+	if (($Path) -and (Test-Path -Path $Path))
+	{
+		$Private:source = $Path
+		$Private:powerEvents = Get-WinEvent -FilterHashtable @{ Path=$Path; id=$Private:id }
+	}
+	else { $Private:powerEvents = Get-WinEvent -ComputerName $Computer -FilterHashtable @{ logname='System'; id=$Private:id } }
+
+	$Private:powerEvents | Sort-Object -Property TimeCreated -Descending:$Descending | ForEach-Object `
 	{
 		[PSCustomObject]@{
-            ID = 1074;
-			Date = $_.TimeCreated; 
+			Location = $Private:source;
+            ID = $Private:id;
+			Date = $_.TimeCreated.Add($TimeMask); 
 			User = $_.Properties[6].Value; 
 			Process = $_.Properties[0].Value; 
 			Action = $_.Properties[4].Value; 
