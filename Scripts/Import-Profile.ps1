912b0ae5c17c71fd486593b5682307c77a186166
ProfX <prof@pxcnet.com>
2020-06-15 21:21:00 -0700

    Parameterized repo root path.

--- Scripts/Import-Profile.ps1
+++ Scripts/Import-Profile.ps1
@@ -1,30 +1,39 @@
+[CmdletBinding()]
+Param
+(
+    [Parameter(Mandatory=$False)]
+    [String]$RepoPath = "G:\Repos"
+)
+
 [String]$Private:skipPattern = '(Import-Profile|Microsoft\.PowerShell_profile)\.ps1'
 
 [String[]]$Modules = (
-    "G:\Repos\POSH-Git\src\posh-git.psd1"
+    "%RepoPath%\POSH-Git\src\posh-git.psd1"
 )
 
 $Modules | ForEach-Object {
-    Write-Host ("Importing external module: '{0}'" -f $_) -ForegroundColor Yellow
-    Import-Module $_ -Force -Scope Global
+    [String]$Private:mPath = $_ -replace "%RepoPath%", $RepoPath
+    Write-Host ("Importing external module: '{0}'" -f $Private:mPath) -ForegroundColor Yellow
+    Import-Module $Private:mPath -Force -Scope Global
 }
 
 [String[]]$ScriptFolders = (
-    "G:\Repos\POSHTools",
-    "G:\Repos\POSHToolsPrivate",
-    "G:\Repos\POSHToolsWork"
+    "%RepoPath%\POSHTools",
+    "%RepoPath%\POSHToolsPrivate",
+    "%RepoPath%\POSHToolsWork"
 )
 
 $ScriptFolders | ForEach-Object {
-    if (Test-Path -Path $_)
+    [String]$Private:mPath = $_ -replace "%RepoPath%", $RepoPath
+    if (Test-Path -Path $Private:mPath)
     {
-        Write-Host ("Importing PowerShell functions from directory: '{0}'" -f $_) -ForegroundColor Yellow
-        Get-ChildItem -Path $_ -Filter *.ps1 -Recurse | Where-Object { $_.Name -notmatch $Private:skipPattern } | ForEach-Object {
+        Write-Host ("Importing PowerShell functions from directory: '{0}'" -f $Private:mPath) -ForegroundColor Yellow
+        Get-ChildItem -Path $Private:mPath -Filter *.ps1 -Recurse | Where-Object { $_.Name -notmatch $Private:skipPattern } | ForEach-Object {
             Write-Host ("-> Loading file: {0}" -f $_.Name)
             Import-Module $_.FullName -Force -Scope Global
         }
     }
-    else { Write-Warning -Message ("Path [ {0} ] not found - Skipping..." -f $_) }
+    else { Write-Warning -Message ("Path [ {0} ] not found - Skipping..." -f $Private:mPath) }
 }
 
 Write-Host "`nCompleted!`n" -ForegroundColor Green
