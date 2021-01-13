e287ed5ca705b1b46d8c4673b163f7e9b59f9061
ProfX <prof@pxcnet.com>
2021-01-12 20:41:06 -0800

    Function to copy images based on resolution

---
+++ Scripts/Find-Images.ps1
@@ -0,0 +1,28 @@
+﻿Function Find-Images()
+{
+    [CmdletBinding()]
+    Param
+	(
+		[Parameter(Mandatory=$True)]
+		[String[]]$SearchPaths,
+		[Parameter(Mandatory=$False)]
+		[String]$CopyPath = "I:\P\Temp1",
+		[Parameter(Mandatory=$False)]
+		[ValidateSet("Landscape", "Portrait", "Square", "Unknown")]
+		[String]$Type = "Landscape",
+		[Parameter(Mandatory=$False)]
+		[Int]$Width = 1024,
+		[Parameter(Mandatory=$False)]
+		[Switch]$OneLevel
+    )
+
+    [Bool]$Recurse = $True
+    if ($OneLevel) { $Recurse = $False }
+
+    foreach ($sp in $SearchPaths)
+    {
+        Write-Warning ("Searching: {0}" -F $sp)
+        $files = Get-ChildItem -Path $sp -Recurse:$Recurse -File -Force | ForEach-Object { Get-ImageInfo -Path $_.FullName -Verbose | Where-Object { ($_.Type -match $Type) -and ($_.Width -gt $Width) } }
+        $files | ForEach-Object { Copy-Item -Path $_.FullName -Destination $copyPath -Force -Verbose }
+    }
+}
